809d087e931f670260492c73ad7f01b9aa14b13f
hborders <hborders@mail.win.org>
2011-11-03 22:40:45 -0500

    Initial commit from Xcode template.

---
+++ HBProgressView/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  HBProgressView
+//
+//  Created by Heath Borders on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HBProgressView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HBProgressView
+//
+//  Created by Heath Borders on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBAppDelegate class]));
+    }
+}
