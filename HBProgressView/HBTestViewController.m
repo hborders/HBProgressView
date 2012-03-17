86458d24a43002327284d58cc66ddfae1dbcd497
hborders <hborders@mail.win.org>
2012-03-16 22:12:21 -0500

    Test the completion layer.

--- HBProgressView/HBTestViewController.m
+++ HBProgressView/HBTestViewController.m
@@ -12,18 +12,24 @@
 @interface HBTestViewController ()
 
 @property (nonatomic, strong) HBProgressView *progressView;
+@property (nonatomic, strong) NSTimer *progressTimer;
 
 @end
 
 @implementation HBTestViewController
 
 @synthesize progressView = _progressView;
+@synthesize progressTimer = _progressTimer;
 
 #pragma mark - init/dealloc
 
 - (id)init {
     return [super initWithNibName:nil
                            bundle:nil];
+}
+
+- (void) dealloc {
+    [self.progressTimer invalidate];
 }
 
 #pragma mark - UIViewController
@@ -36,16 +42,48 @@
                                                       CGRectGetWidth(self.view.bounds) / 16, 
                                                       CGRectGetHeight(self.view.bounds) / 4)];
     [self.view addSubview:self.progressView];
+    
+    UIButton *showProgressButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [showProgressButton addTarget:self 
+                           action:@selector(startProgressTimer)
+                 forControlEvents:UIControlEventTouchUpInside];
+    showProgressButton.frame = CGRectMake(CGRectGetWidth(self.view.bounds) / 16,
+                                          CGRectGetHeight(self.view.bounds) * 7 / 8,
+                                          CGRectGetWidth(self.view.bounds) * 7 / 8, 
+                                          CGRectGetHeight(self.view.bounds) * 1 / 16);
+    [showProgressButton setTitle:@"Show Progress" 
+                        forState:UIControlStateNormal];
+    [self.view addSubview:showProgressButton];
 }
 
 - (void)viewDidUnload {
     self.progressView = nil;
+    [self.progressTimer invalidate];
     
     [super viewDidUnload];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - private API
+
+- (void) startProgressTimer {
+    self.progressView.completionFactor = 0;
+    self.progressTimer = [NSTimer scheduledTimerWithTimeInterval:.2
+                                                          target:self
+                                                        selector:@selector(updateProgress)
+                                                        userInfo:nil 
+                                                         repeats:YES];
+}
+
+- (void) updateProgress {
+    self.progressView.completionFactor += .1;
+    if (self.progressView.completionFactor >= 1) {
+        self.progressView.completionFactor = 1;
+        [self.progressTimer invalidate];
+    }
 }
 
 @end
